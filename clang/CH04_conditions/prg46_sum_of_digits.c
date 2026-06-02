4d83ea0c37525eae879d0d12663d6ff3e9210ff7
Jiks-cloud <jiyakshah1@gmail.com>
2026-06-02 16:38:21 +0530

    Add files via upload

--- clang/CH04_conditions/prg46_sum_of_digits.c
+++ clang/CH04_conditions/prg46_sum_of_digits.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main(){
-    int n, rem, sum=0, count=0;
+    int n, rem, sum=0;
     printf("Enter number: ");
     scanf("%d",&n);
-    while(n>0){
+    while(n!=0){
         rem = n%10;
-        sum = rem+n;
-        count++;
+        sum = sum + rem;
+        n /= 10;
     }
-    printf("Sum of number %d: is %d",n,sum);
+    printf("Sum of number is %d",sum);
 }
