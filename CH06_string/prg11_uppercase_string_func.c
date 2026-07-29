04fa653c43988ec2b48305bca20f77d4d0f25c87
Jiks-cloud <jiyakshah1@gmail.com>
2026-07-29 12:27:55 +0530

    Delete CH06_string directory

--- CH06_string/prg11_uppercase_string_func.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-void main(){
-    char st[100];
-    printf("Enter String: ");
-    scanf("%s", st);
-    strupr(st);
-    printf("String is : %s",st);
-}
--- CH06_string/prg13_lowercase_string_func.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-void main(){
-    char st[100];
-    printf("Enter String: ");
-    scanf("%s", st);
-    strlwr(st);
-    printf("String is : %s",st);
-}
