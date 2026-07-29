983d8e98e9b056ad18011cd3bb09db6a2bb60b49
Jiks-cloud <jiyakshah1@gmail.com>
2026-07-29 12:26:16 +0530

    Delete CH10_file directory

--- CH10_file/prg02_write.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    FILE *fp;
-    fp = fopen("student.txt","w");
-    fprintf(fp,"Hello Students!!");
-    printf("Text added in file!!..");
-    fclose(fp);
-    return 0;
-}
--- CH10_file/prg03_append.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    FILE *fp;
-    fp = fopen("student.txt","a");
-    fprintf(fp,"\nWelcome to computer!!");
-    printf("New text added in file!!");
-    fclose(fp);
-    return 0;
-}
