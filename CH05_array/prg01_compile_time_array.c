2c0eaa5444ce1f21f3e3fbd2d596008bd2ea067b
Jiks-cloud <jiyakshah1@gmail.com>
2026-07-29 12:25:38 +0530

    Delete CH05_array directory

--- CH05_array/prg01_compile_time_array.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-    int a[5] = {23, 56, 65, 75, 84};
-    int i;
-    printf("Array of Data: \n");
-    for (i = 0; i < 5; i++){
-        printf("a[%d] : %d \n",i,a[i]);
-    }
-}
